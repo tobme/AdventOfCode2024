87b038850694afb269d379ffc1a362261932e622
Tobbe <tobbemellberg@hotmail.se>
2024-12-04 13:15:23 +0100

    day 4

--- AdventOfCode2024/Helpers/InputTransformer.cpp
+++ AdventOfCode2024/Helpers/InputTransformer.cpp
@@ -27,6 +27,30 @@ std::vector<std::vector<T>> ToMatrix(std::shared_ptr<IInput>& input, std::functi
     return matrix;
 }
 
+template<>
+std::vector<std::vector<char>> ToMatrix(std::shared_ptr<IInput>& input, std::function<char(std::string)> func, const std::string& delimeter)
+{
+    input->resetStream();
+
+    std::vector<std::vector<char>> matrix;
+
+    std::string s;
+
+    while (std::getline(*input, s))
+    {
+        std::vector<char> row{};
+
+        for (char c : s)
+        {
+            row.push_back(c);
+        }
+
+        matrix.push_back(row);
+    }
+
+    return matrix;
+}
+
 template std::vector<std::vector<int>> ToMatrix<int>(std::shared_ptr<IInput>& input, std::function<int(std::string)> func, const std::string& delimeter);
 template std::vector<std::vector<double>> ToMatrix<double>(std::shared_ptr<IInput>& input, std::function<double(std::string)> func, const std::string& delimeter);
 template std::vector<std::vector<char>> ToMatrix<char>(std::shared_ptr<IInput>& input, std::function<char(std::string)> func, const std::string& delimeter);
