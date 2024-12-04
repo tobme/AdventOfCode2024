945d63bc87c7848185b03ac1866183ad8a29cc02
Tobbe <tobbemellberg@hotmail.se>
2024-12-04 15:12:05 +0100

    Removed inputdata

--- AdventOfCode2024/FetchInput.py
+++ AdventOfCode2024/FetchInput.py
@@ -11,9 +11,10 @@ print(year)
 print(day)
 
 path = str(day) + "/InputData.txt"
+f = open("session.txt", "r")
 
 if (os.path.isfile(path) is False):
-    cookies = {'session': '53616c7465645f5fc2f3de2c4bf1edba8c9e185da767de9ba6df45cc3d8ddaddd582c1f5321107322db4a832dc0a92156440856f3da902f86fac00c0081326a4'}
+    cookies = {'session': f.read()}
 
     url = f"https://adventofcode.com/{year}/day/{day}/input"
 
